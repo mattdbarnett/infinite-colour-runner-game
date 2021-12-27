97fda1810bc9089c5bb49a0cbd10593d94261cb8
Matt <barnettmd@cardiff.ac.uk>
2021-12-27 00:29:48 +0000

    Made random transitions play each time

--- 1 Scripts/transitionscript.gd
+++ 1 Scripts/transitionscript.gd
@@ -8,28 +8,41 @@ var currentMenu;
 var transitionedOut = false
 var menu = load("res://1 Scripts/menuscript.gd")
 
+var shaderMat = load("res://4 Styling/8 Shaders/transition.tres")
+const transition1 = preload("res://2 Sprites/1 Transitions/transition1.png")
+const transition2 = preload("res://2 Sprites/1 Transitions/transition2.png")
+const transition3 = preload("res://2 Sprites/1 Transitions/transition3.png")
+const transition4 = preload("res://2 Sprites/1 Transitions/transition4.png")
+var transitionArray = [transition1, transition2, transition3, transition4]
+var usedTransitions = []
+var currentTransition
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rect.visible = false
 
-func startIn():
-	$AnimationPlayer.play("transition_in")
-
-func startOut():
-	$AnimationPlayer.play("transition_out")
+func changeMask():
+	currentTransition = transitionArray[randi() % transitionArray.size()]
+	transitionArray.erase(currentTransition)
+	usedTransitions.append(currentTransition)
+	print(transitionArray)
+	shaderMat.set_shader_param("mask", currentTransition)
 
 func play(transitionIn, transitionOut, menu):
 	rect.visible = true
 	currentIn = transitionIn
 	currentOut = transitionOut
 	currentMenu = menu
+	changeMask()
 	player.play(transitionOut)
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == currentOut:
 		transitionedOut = true
+		changeMask()
+		transitionArray += usedTransitions
+		usedTransitions.clear()
 		player.play(currentIn)
 	elif anim_name == currentIn:
 		transitionedOut = false
